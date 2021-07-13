501432cf053b48a9b4b036ddbf25115efdbd61b9
Michael <mickeymakesapps88@gmail.com>
2021-07-13 11:18:56 -0700

    completed project

--- project-4/script.js
+++ project-4/script.js
@@ -1,1 +1,8 @@
-console.log('Hello World, from "script.js"!');
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+});
